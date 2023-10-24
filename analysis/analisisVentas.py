16c01ab78a3ac9f2f0315059dc1c31f901271ee6
309 <309@academicos.cesde.edu.co>
2023-10-24 08:12:05 -0500

    feat: Se crea tabla html para mostrar datos

--- analysis/analisisVentas.py
+++ analysis/analisisVentas.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from helpers.crearCsv import crearCSV
 from data.ventas import ventas, encabezados
+from helpers.crearTablaHTML import crearTabla
 
 #crear un CSV con los datos
 crearCSV(ventas, 'venta.csv', encabezados)
@@ -8,6 +9,11 @@ crearCSV(ventas, 'venta.csv', encabezados)
 # cargo la fuente de datos y cro un dataframe
 ventasDataFrame = pd.read_csv('data/venta.csv')
 #print(ventasDataFrame)
+
+crearTabla(ventasDataFrame, 'tablaventas')
+
+
+
 
 # explorar datos 
 examenUno = ventasDataFrame.head()
@@ -17,17 +23,6 @@ examenCuatro = ventasDataFrame.info()
 examenCinco = ventasDataFrame.describe()
 examenSeis = ventasDataFrame.tail(50)
 
-
 # presentar y exportar los datos
-print(examenUno)
-print("\n")
-print(examenDos)
-print("\n")
-print(examentres)
-print("\n")
-print(examenCuatro)
-print("\n")
-print(examenCinco)
-print("\n")
-print(examenSeis)
-print("\n")
+
+
